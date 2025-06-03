7261ca42bd2fca5a7953ae14713ff698ab3fdaaa
zsevchick@gmail.com <zsevchick@gmail.com>
2025-06-03 22:15:43 +0500

    Добавлены тесты для финального задания в final_task/tests

--- lab4/create_dataset.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from catboost.datasets import titanic
-
-train, test = titanic()
-dataset = train[['Pclass', 'Sex', 'Age']]
-dataset.to_csv('datasets/titanic.csv', index=False)
--- lab4/one_hot_encoding.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-dataset = pd.read_csv('datasets/titanic.csv')
-dataset = pd.get_dummies(dataset, columns=['Sex'], drop_first=True)
-dataset.to_csv('datasets/titanic.csv', index=False)
